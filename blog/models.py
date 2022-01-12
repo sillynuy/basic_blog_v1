15a0a4c96ece282007e648ec266a60502d0788e7
Nikita <nsnuikin@gmail.com>
2022-01-12 17:51:15 +0700

    изменена модель Comment, изменён атрибут author

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -12,7 +13,8 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.CharField(max_length=40, default=None)
+    # author = models.CharField(max_length=40, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     text = models.CharField(max_length=140)
     date_published = models.DateTimeField()
     pos_marks = models.IntegerField(default=0)
